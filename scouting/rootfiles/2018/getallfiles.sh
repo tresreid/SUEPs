6817eaf039beb1d4fe3dabb1fd1800b12b39f785
tresreid <tres.reid@gmail.com>
2023-02-10 08:15:36 -0600

    some qcd files

--- scouting/rootfiles/2018/getallfiles.sh
+++ scouting/rootfiles/2018/getallfiles.sh
@@ -3,6 +3,6 @@
 files=(200 300 500 700 1000 1500 2000)
 
 for i in ${files[@]}; do
-  eosls /store/group/lpcsuep/Scouting/QCDv4/2018/HT${i} > HT${i}v4.txt 
+  eosls /store/group/lpcsuep/Scouting/QCDv7/2018/HT${i} > HT${i}v7.txt 
 done
 #eosls /store/group/lpcsuep/Scouting/Data/RunA > DataRunA.txt 
